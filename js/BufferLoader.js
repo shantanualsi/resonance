0aafc756a728aacf4114f3c1eb33acefa7b90b20
Shantanu Alshi <shantanuals@gmail.com>
2015-01-13 11:17:51 +0800

    Removing Buffer loader for multiple files

--- js/BufferLoader.js
+++ js/BufferLoader.js
@@ -39,7 +39,5 @@ BufferLoader.prototype.loadBuffer = function(url, index){
 
 
 BufferLoader.prototype.load = function(){
-    // for (var i =0; i<this.urllist.length; ++i) {
         this.loadBuffer(this.urllist,0);
-    // }
 }
