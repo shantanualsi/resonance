6b0cf668d583798db2794b26ef3f1ba6600a17e1
Shantanu Alshi <shantanuals@gmail.com>
2015-02-26 23:33:49 +0800

    Fixed audio play issues with the new Audio object

--- js/Audio.js
+++ js/Audio.js
@@ -1,33 +1,37 @@
-function Audio (filename, context, ) {
+function Audio (context) {
 	this.context = context;
-
 }
 
-Audio.prototype.setupProperties = function(filename) {
+Audio.prototype.setupProperties = function(bufferList, audioObj) {
+	console.log(bufferList);
+	console.trace();
 	try{
-		source = context.createBufferSource();
-		source.buffer = filename;
-		source.loop = false;
+		audioObj.source = context.createBufferSource();
+		audioObj.source.buffer = bufferList[0];
+		audioObj.source.loop = false;
+		audioObj.source.connect(context.destination);
 	}catch(e){
 		toast("Some error occured while starting audio");
 	}
-};
+}
 
 Audio.prototype.load = function(filename) {
-	bufferLoader = new BufferLoader(this.context,filename, setupProperties)
-};
+	console.log(this);
+	this.bufferLoader = new BufferLoader(this.context,filename, this, this.setupProperties);
+	this.bufferLoader.load();
+}
 
 Audio.prototype.changeVolume = function(element) {
 	var fraction = parseInt(element.val()) / parseInt(element.max);
 	this.source.volume.gain.value = vol;
-};
+}
 
-Audio.prototype.play = function(filename){
-	source.start(0);
+Audio.prototype.play = function(){
+	this.source.start(0);
 }
 
 Audio.prototype.stop = function() {
 	if(this.source){
-		source.stop();
+		this.source.stop();
 	}
 }
